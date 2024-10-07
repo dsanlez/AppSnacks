49efcf853f1b7c75daaaa28e8b0e4f06c329b8da
Diogo Sanlez <diogosdl25@gmail.com>
2024-10-07 10:37:19 +0100

    Add project files.

---
+++ AppSnacks/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using AppLanches.Pages;
+using AppLanches.Services;
+
+namespace AppLanches;
+
+public partial class App : Application
+{
+    private readonly ApiService _apiService;
+
+    public App(ApiService apiService)
+    {
+        InitializeComponent();
+        _apiService = apiService;
+        MainPage = new NavigationPage(new InscricaoPage(_apiService));
+      
+    }
+}
